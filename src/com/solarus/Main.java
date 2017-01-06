c601361c61b013e22a85215f7def945c3f009360
Antoine <fallenhaswon@gmail.com>
2017-01-06 11:46:36 +0100

    Ajout d'une classe pour implémenter la souris

--- src/com/solarus/Main.java
+++ src/com/solarus/Main.java
@@ -3,6 +3,8 @@ package com.solarus;
 import edu.princeton.cs.introcs.StdDraw;
 import java.util.Scanner;
 
+
+
 public class Main {
 
     public final static int X_MAX= 20;
@@ -11,22 +13,70 @@ public class Main {
 
 
     public static void main (String [] args) {
-        System.out.println("taille du goban ?");
         Scanner sc = new Scanner(System.in);
-        int taille_goban= sc.nextInt();
+        int taille_goban = 0;
+
+        System.out.println("taille du goban ? 9,13,19");
+        taille_goban = sc.nextInt();
+
 
 
 
 
         // RELATIF A LA FENETRE ET AUX AXES QQQUE L ON UTILISERA TOUT AU COURS DU PROGRAMME
-        StdDraw.setCanvasSize(700,700);
+        StdDraw.setCanvasSize(700, 700);
         StdDraw.setXscale(-WIDTH, X_MAX + WIDTH);
         StdDraw.setYscale(-WIDTH, Y_MAX + WIDTH);
 
-        for (int y = 1; y <= 19; ++y) {
-            StdDraw.line(1, y, 19, y);
-            StdDraw.line(y, 1, y, 19);
-        }
+        //StdDraw.filledPolygon();
+
+        int min, max;
+        switch (taille_goban) {
+            case 9:
+                min = 6;
+                max = 14;
+                for (int y = min; y <= max; ++y) {
+                    StdDraw.line(min, y, max, y);
+                    StdDraw.line(y, min, y, max);
+                    StdDraw.filledCircle(10,10,0.1);
+                }
+                break;
+            case 13:
+                min = 4;
+                max = 16;
+                for (int y = min; y <= max; ++y) {
+                    StdDraw.line(min, y, max, y);
+                    StdDraw.line(y, min, y, max);
+                    StdDraw.filledCircle(7,7,0.1);
+                    StdDraw.filledCircle(7,10,0.1);
+                    StdDraw.filledCircle(7,13,0.1);
+                    StdDraw.filledCircle(10,7,0.1);
+                    StdDraw.filledCircle(10,10,0.1);
+                    StdDraw.filledCircle(10,13,0.1);
+                    StdDraw.filledCircle(13,13,0.1);
+                    StdDraw.filledCircle(13,1013,0.1);
+                    StdDraw.filledCircle(13,7,0.1);
+
+                }
+                break;
+            case 19:
+                min = 1;
+                max = 19;
+                for (int y = min; y <= max; ++y) {
+                    StdDraw.line(min, y, max, y);
+                    StdDraw.line(y, min, y, max);
+                    StdDraw.filledCircle(4,4,0.1);
+                    StdDraw.filledCircle(4,10,0.1);
+                    StdDraw.filledCircle(4,16,0.1);
+                    StdDraw.filledCircle(10,4,0.1);
+                    StdDraw.filledCircle(10,10,0.1);
+                    StdDraw.filledCircle(10,16,0.1);
+                    StdDraw.filledCircle(16,4,0.1);
+                    StdDraw.filledCircle(16,10,0.1);
+                    StdDraw.filledCircle(16,16,0.1);
+                }
+                break;
         }
 
+    }
 }
